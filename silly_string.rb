6552c6ad74f9197eeae6487ad2a6342bf232df93
maruleba <folaa1234@gmail.com>
2018-08-06 19:11:03 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -13,7 +13,7 @@ puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
 phrase = "IM NOT SHOUTING"
 #"{"I'M NOT SHOUTING".downcase"}""
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = phrase.downcase# Put a . after phrase and use a String method.
 
 puts lowercased_phrase
 
