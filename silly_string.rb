b9e724d6bee1c1f2ec5a2b04e8228d59d613e4da
maruleba <folaa1234@gmail.com>
2018-08-06 19:33:37 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -43,7 +43,7 @@ puts "#{proper_sentence.capitalize}"
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
-
+puts # "{sentence.reverse}"
 sentence = sentence # Add your gsub chain here
 
 puts sentence
